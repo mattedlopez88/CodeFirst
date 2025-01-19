173a80ea10e746586ecd7a835ecc95884941e4d9
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2025-01-19 18:29:11 -0500

    Add project files.

---
+++ CodeFirst/Data/CodeFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeFirst.Models;
+
+namespace CodeFirst.Data
+{
+    public class CodeFirstContext : DbContext
+    {
+        public CodeFirstContext (DbContextOptions<CodeFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeFirst.Models.Burger> Burger { get; set; } = default!;
+    }
+}
